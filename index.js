7f0c099c51ae1d5a2b9a6683201c6fbe8222fe1e
Mohammad <mo7amed.2197@gmail.com>
2023-10-10 18:03:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Please read the README.md to understand what to do. Happy Coding !
+async function fetchUserData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await response.json();
+    const modifiedUsersData = usersData.map(obj => {
+        const newObj = {};
+        newObj["name"] = obj.name;
+        newObj["username"] = obj.username;
+        newObj["email"] = obj.email;
+        newObj["address"] = obj.address.street;
+        newObj["phone"] = obj.phone;
+        newObj["website"] = obj.website;
+        newObj["company"] = obj.company.name;
+
+        return newObj
+    });
+    //console.log(modifiedUsersData);
+    const filteredUsers = modifiedUsersData.filter(usr => usr.email.endsWith('.biz'));
+    //console.log(filteredUsers);
+    const sortedUsers = filteredUsers.sort((a,b) => (a.name > b.name) ? 1 : -1 );
+    console.log(sortedUsers);
+}
+
+fetchUserData()
